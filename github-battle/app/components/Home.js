4b2145c428ab5c81ee9cfb894b7bda073145db41
you <you@example.com>
2017-05-11 20:48:12 -0300

    home component

---
+++ github-battle/app/components/Home.js
@@ -0,0 +1,15 @@
+var Reac = require('react');
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className='home-container'>
+        <h1>
+          Github Battle: ... battle your friends and stuff
+        </h1>
+      </div>
+    );
+  }
+}
+
+module.exports = Home;
