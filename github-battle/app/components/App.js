60b8f0b5c1c226ce8834a154c2efe734a4adafb1
you <you@example.com>
2017-05-18 21:04:03 -0300

    battle file + methods battle

--- github-battle/app/components/App.js
+++ github-battle/app/components/App.js
@@ -7,6 +7,7 @@ var Nav = require('./Nav');
 var Popular = require('./Popular');
 var Home = require('./Home');
 var Battle = require('./Battle');
+var Results = require('./Results');
 
 class App extends React.Component {
   render() {
@@ -18,6 +19,7 @@ class App extends React.Component {
             <Route exact path='/' component={Home} />
             <Route exact path='/battle' component={Battle} />
             <Route path='/popular' component={Popular} />
+            <Route path='/battle/results' component={Results} />
             <Route render={function() {
               return <p>Not Found</p>
             }} />
